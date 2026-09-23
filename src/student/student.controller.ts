27f7d75dcb643143dc25dc7cf80da846e5272555
rabeet-k1 <rabeet.k1@gmail.com>
2026-09-23 14:35:30 +0500

    student crud done

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './student.schema';
 
@@ -9,5 +18,30 @@ export class StudentController {
   @Post()
   async addStudent(@Body() data: Partial<Student>) {
     return this.studentService.createStudent(data);
+  }
+
+  @Get()
+  getAllStudents() {
+    return this.studentService.getAllStudents();
+  }
+
+  @Get(':id')
+  getStudentById(@Param('id') id: string) {
+    return this.studentService.getStudentById(id);
+  }
+
+  @Put(':id')
+  updateStudent(@Param('id') id: string, @Body() data: Partial<Student>) {
+    return this.studentService.updateStudent(id, data);
+  }
+
+  @Patch(':id')
+  patchStudent(@Param('id') id: string, @Body() data: Partial<Student>) {
+    return this.studentService.patchStudent(id, data);
+  }
+
+  @Delete(':id')
+  deleteStudent(@Param('id') id: string) {
+    return this.studentService.deleteStudent(id);
   }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Student, StudentDocument } from './student.schema';
 import { Model } from 'mongoose';
+import { after } from 'node:test';
 
 @Injectable()
 export class StudentService {
@@ -11,7 +12,50 @@ export class StudentService {
 
   async createStudent(data: Partial<Student>): Promise<Student> {
     const newStudent = new this.studentModel(data);
-    console.log(newStudent,"newStudentnewStudentnewStudent");
     return newStudent.save();
+  }
+
+  async getAllStudents(): Promise<Student[]> {
+    return this.studentModel.find().exec(); // use exec() so it will handle promise efficiently
+  }
+
+  async getStudentById(id: string): Promise<Student | null> {
+    return this.studentModel.findById(id).exec(); // use exec() so it will handle promise efficiently
+  }
+
+  async updateStudent(
+    id: string,
+    data: Partial<Student>,
+  ): Promise<Student | null> {
+    // Patch Logic
+    // return this.studentModel
+    //   .findByIdAndUpdate(id, data, { returnDocument: 'after' })
+    //   .exec(); // use exec() so it will handle promise efficiently
+
+    // Actual Put & Recommended Logic
+    const updated = await this.studentModel.findByIdAndUpdate(
+      id,
+      {
+        name: data.name ?? null,
+        age: data.age ?? null,
+        email: data.email ?? null,
+      },
+      { overwriteDiscriminatorKey: true, returnDocument: 'after' },
+    ).exec();
+
+    return updated;
+  }
+
+  async patchStudent(
+    id: string,
+    data: Partial<Student>,
+  ): Promise<Student | null> {
+    return this.studentModel
+      .findByIdAndUpdate(id, data, { returnDocument: 'after' })
+      .exec(); // use exec() so it will handle promise efficiently
+  }
+
+  async deleteStudent(id: string): Promise<Student | null> {
+    return this.studentModel.findByIdAndDelete(id).exec();
   }
 }
